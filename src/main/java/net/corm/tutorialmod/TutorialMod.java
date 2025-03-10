28a1a22eee2c34fd5a16f898142b94e3ec692398
Isaac Cormier <ikcorm@iastate.edu>
2025-03-10 15:03:01 -0500

    Very important changes

--- src/main/java/net/corm/tutorialmod/TutorialMod.java
+++ src/main/java/net/corm/tutorialmod/TutorialMod.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
+//Very important commet
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
